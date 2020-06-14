c1a64de1d5a770cfed95ffbbc7a47569cbbdc1df
camilo6 <trresqwe@gmail.com>
2020-06-14 19:52:49 +0000

    quote

--- 0x03-git/101-quote.c
+++ 0x03-git/101-quote.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
+/**
+ * main - entry
+ * return - 0 always
+ */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-	return(1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
 }
